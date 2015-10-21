ec6ecc36d078b5ccd3f37b344ba2ef21d61e20b0
Linus Schoemaker <linus@clipperhq.com>
2015-10-21 15:35:57 +0200

    Finish homework 3 for 2013

--- 2013/3/Golf.hs
+++ 2013/3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List (transpose, intercalate)
+
 -- Exercise 1 --
 skip :: Integer -> [a] -> [a]
 skip n list = [x | (x,y) <- zip list [1..], y `mod` n == 0]
@@ -16,6 +18,14 @@ localMaxima (x:y:z:xs)
 localMaxima _ = []
 
 -- Exercise 3 --
+
+line :: (Eq a) => [a] -> (a -> a -> Bool) -> a -> String 
+line list comp n = replicate x '*' ++ replicate (9 - x) ' '
+  where x = length $ filter (\x -> x `comp` n) list
+
+nonEmptyString :: String -> Bool
+nonEmptyString str = length ((filter (\x -> x /= ' ')) str) > 0 
+
 histogram :: [Integer] -> String
-histogram list = 
+histogram list = (intercalate "\n" $ reverse $ filter nonEmptyString $ transpose $ map (line list (==)) [0..9]) ++ "\n==========\n0123456789\n"
 
